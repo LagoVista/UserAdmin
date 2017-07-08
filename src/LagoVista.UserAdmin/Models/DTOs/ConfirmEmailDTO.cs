12777737605759d932ef8a3029893f42a214fc38
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-07-08 09:32:57 -0400

    add method to confirm email code

---
+++ src/LagoVista.UserAdmin/Models/DTOs/ConfirmEmailDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.UserAdmin.Models.DTOs
+{
+    public class ConfirmEmailDTO
+    {
+        public String ReceivedCode { get; set; }
+    }
+}
