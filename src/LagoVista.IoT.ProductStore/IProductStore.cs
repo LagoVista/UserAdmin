98e641658e9b6ef15f4d1decf7c8f747463736ad
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-07-28 08:28:01 -0400

    added product store

---
+++ src/LagoVista.IoT.ProductStore/IProductStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.IoT.ProductStore
+{
+    public interface IProductStore
+    {
+        Task<IEnumerable<ProductOffering>> GetProductsAsync(string categoryKey);
+    }
+}
---
+++ src/LagoVista.IoT.ProductStore/ProductOffering.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LagoVista.IoT.ProductStore
+{
+    public class ProductOffering
+    {
+        public string Id { get; set; }
+
+        public string ProductCategoryKey { get; set; }
+        public string Name { get; set; }
+
+        public string SKU { get; set; }
+
+        public string DetailsHTML { get; set; }
+
+        public string Description { get; set; }
+
+        public string RemoteResourceId { get; set; }
+
+        public decimal UnitCost { get; set; }
+
+        public decimal UnitType { get; set; }
+    }
+}
