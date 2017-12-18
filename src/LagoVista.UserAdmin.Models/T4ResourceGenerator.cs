79dd10e54bc84f5388a5caf31322481be2a58843
Kevin D. Wolf <kevinw@software-logistics.com>
2017-12-18 18:04:13 -0500

    fix web build after models refactor

--- src/LagoVista.UserAdmin.Models/T4ResourceGenerator.cs
+++ src/LagoVista.UserAdmin.Models/T4ResourceGenerator.cs
@@ -18,7 +18,7 @@ namespace LagoVista.UserAdmin.Models.Resources
 			{
                 if (object.ReferenceEquals(_resourceManager, null)) 
 				{
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LagoVista.UserAdmin.Models.UserAdminResources", typeof(UserAdminResources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LagoVista.UserAdmin.Models.Resources.UserAdminResources", typeof(UserAdminResources).GetTypeInfo().Assembly);
                     _resourceManager = temp;
                 }
                 return _resourceManager;
