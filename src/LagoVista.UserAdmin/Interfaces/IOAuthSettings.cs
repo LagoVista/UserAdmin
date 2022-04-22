7ad20eeca7d1cc3eef49810591abb4389a288528
Kevin Wolf <kevinw@software-logistics.com>
2022-04-22 05:54:39 -0400

    add auth settings

---
+++ src/LagoVista.UserAdmin/Interfaces/IOAuthSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace LagoVista.UserAdmin
+{
+    public interface IOAuthSettings
+    {
+        string Redirect { get;  }
+        OAuthConfig GitHubOAuth { get; }
+        OAuthConfig LinkedinOAuth { get;  }
+        OAuthConfig MicrosoftOAuth { get; }
+        OAuthConfig GoogleOAuth { get;  }
+    }
+
+    public class OAuthConfig
+    {
+        public OAuthConfig(string clientId, string secret)
+        {
+            ClientId = clientId;
+            Secret = secret;
+        }
+
+        public string ClientId { get;  }
+        public string Secret { get; }   
+    }
+}
