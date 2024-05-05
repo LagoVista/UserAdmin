ca3584e5d9160479d240244d4a2635d88cfe70f2
Kevin Wolf <kevinw@software-logistics.com>
2024-05-05 09:07:02 -0400

    more refeactor for user auth/login

--- src/LagoVista.UserAdmin/Interfaces/Managers/IUserVerficationManager.cs
+++ src/LagoVista.UserAdmin/Interfaces/Managers/IUserVerficationManager.cs
@@ -7,11 +7,11 @@ namespace LagoVista.UserAdmin.Interfaces.Managers
 {
     public interface IUserVerficationManager
     {
-        Task<InvokeResult> CheckConfirmedAsync(EntityHeader orgHeader, EntityHeader userHeader);
-        Task<InvokeResult<string>> SendConfirmationEmailAsync(EntityHeader orgHeader, EntityHeader userHeader);
-        Task<InvokeResult<string>> SendSMSCodeAsync(VerfiyPhoneNumber sendSMSCode, EntityHeader orgHeader, EntityHeader userHeader);
+        Task<InvokeResult> CheckConfirmedAsync(EntityHeader userHeader);
+        Task<InvokeResult<string>> SendConfirmationEmailAsync(EntityHeader userHeader);
+        Task<InvokeResult<string>> SendSMSCodeAsync(VerfiyPhoneNumber sendSMSCode, EntityHeader userHeader);
         Task<InvokeResult> ValidateSMSAsync(VerfiyPhoneNumber verifyRequest, EntityHeader userHeader);
         Task<InvokeResult> ValidateEmailAsync(ConfirmEmail confirmemaildto, EntityHeader userHeader);
-        Task<InvokeResult> SetUserSMSValidated(string userId, EntityHeader orgHeader, EntityHeader userHeader);
+        Task<InvokeResult> SetUserSMSValidated(string userId, EntityHeader userHeader);
     }
 }
