cd6f15cc6d7bb39c7721bffac2edc3bf89927130
Kevin D. Wolf <kevinw@software-logistics.com>
2024-12-06 06:58:43 -0500

    start work on secure link

---
+++ src/LagoVista.UserAdmin.Models/Security/SecureLink.cs
@@ -0,0 +1,18 @@
+﻿using LagoVista.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.UserAdmin.Models.Security
+{
+    public class SecureLink : TableStorageEntity
+    {
+        public string OrgId { get; set; } 
+        public string OrgName { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string DestinationLink { get; set; }
+        public string Generator { get; set; }
+        public string Expires { get; set; }
+    }
+}
---
+++ src/LagoVista.UserAdmin.Repos/Repos/Security/SecureLinkRepo.cs
@@ -0,0 +1,16 @@
+﻿using LagoVista.CloudStorage.Storage;
+using LagoVista.IoT.Logging.Loggers;
+using LagoVista.UserAdmin.Models.Security;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.UserAdmin.Repos.Repos.Security
+{
+    public class SecureLinkRepo : TableStorageBase<SecureLink>
+    {
+        public SecureLinkRepo(IUserAdminSettings settings, IAdminLogger logger) : base(accountName, accountKey, logger)
+        {
+        }
+    }
+}
