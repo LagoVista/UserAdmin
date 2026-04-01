8e95466c0d73f0c90fd5623bbb4816d04ecba6f6
Kevin D. Wolf <kevinw@software-logistics.com>
2026-04-01 11:22:40 -0400

    rework token auth.

--- src/LagoVista.AspNetCore.Identity/Models/TokenAuthOptions.cs
+++ src/LagoVista.AspNetCore.Identity/Models/TokenAuthOptions.cs
@@ -1,7 +1,6 @@
 using LagoVista;
 using LagoVista.AspNetCore.Identity.Interfaces;
 using LagoVista.Core.Attributes;
-using LagoVista.UserAdmin.Interfaces;
 using LagoVista.UserAdmin.Models;
 using LagoVista.UserAdmin.Models.Resources;
 using Microsoft.Extensions.Configuration;
@@ -24,14 +23,27 @@ public class TokenAuthOptions : ITokenAuthOptions
         Audience = section.Require("Audience");
         Issuer = section.Require("Issuer");
         var SecretKey = section.Require("SecretKey");
-        var accessExpires = section.GetValue<int>("AccessTokenExpirationMinutes", 60);
-        var refreshExpires = section.GetValue<int>("RefreshTokenExpirationDays", 7);
+        var accessExpires = section.Require("AccessTokenExpirationMinutes");
+        var refreshExpires = section.Require("RefreshTokenExpirationDays");
+
+        // If any of these required value are missing startup won't complete so we don't need to set these values.
+        if (String.IsNullOrEmpty(accessExpires) || string.IsNullOrEmpty(refreshExpires) || string.IsNullOrEmpty(SecretKey))
+        { 
+            return;
+        }
+
+        if (int.TryParse(accessExpires, out var accessExpiresMinutes))
+            AccessExpiration = TimeSpan.FromMinutes(accessExpiresMinutes);
+        else
+            AccessExpiration = TimeSpan.FromMinutes(15);
+
+        if(int.TryParse(refreshExpires, out var refreshExpiresDays))
+            RefreshExpiration = TimeSpan.FromDays(refreshExpiresDays);
+        else
+            RefreshExpiration = TimeSpan.FromDays(7);
 
         var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
         SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
-
-        AccessExpiration = TimeSpan.FromMinutes(accessExpires);
-        RefreshExpiration = TimeSpan.FromMinutes(refreshExpires);
     }
 
     public string Path { get; set; }
