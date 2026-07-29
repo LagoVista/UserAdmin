67cd6b6d596a5b38af4c02a1b8a5b2429827a4b0
Kevin D. Wolf <kevinw@software-logistics.com>
2026-07-29 14:20:08 -0400

    Implement password recovery request flow handler

---
+++ src/LagoVista.UserAdmin/Authentication/Flows/PasswordRecoveryRequestFlowHandler.cs
@@ -0,0 +1,26 @@
+using LagoVista.UserAdmin.Interfaces.Managers;
+using System;
+using System.Threading.Tasks;
+
+namespace LagoVista.UserAdmin.Authentication.Flows
+{
+    public class PasswordRecoveryRequestFlowHandler : IAuthenticationFlowHandler<PasswordRecoveryRequestFlowRequest>
+    {
+        public const string TransitionKey = "auth.transition.recovery.request";
+
+        private readonly IPasswordManager _passwordManager;
+
+        public PasswordRecoveryRequestFlowHandler(IPasswordManager passwordManager)
+        {
+            _passwordManager = passwordManager ?? throw new ArgumentNullException(nameof(passwordManager));
+        }
+
+        public async Task<AuthenticationFlowResult> HandleAsync(PasswordRecoveryRequestFlowRequest request)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            var result = await _passwordManager.SendResetPasswordLinkAsync(request.Request);
+            return new AuthenticationFlowResult(TransitionKey, result);
+        }
+    }
+}
