2a90cc9569cf1fa141056683f9bc38334f324c6f
Kevin D. Wolf <kevinw@software-logistics.com>
2026-07-29 16:16:46 -0400

    Use clean authentication response for sign-in manager

--- src/LagoVista.UserAdmin/Interfaces/Managers/ISigninManager.cs
+++ src/LagoVista.UserAdmin/Interfaces/Managers/ISigninManager.cs
@@ -1,7 +1,3 @@
-// --- BEGIN CODE INDEX META (do not edit) ---
-// ContentHash: a41c2ef281534cbf5a7bccbf5ef25b437967e6caed3e8bcdb928b3a4c1943bea
-// IndexVersion: 2
-// --- END CODE INDEX META ---
 using LagoVista.Core.Validation;
 using LagoVista.UserAdmin.Models.Auth;
 using LagoVista.UserAdmin.Models.Users;
@@ -12,21 +8,10 @@ namespace LagoVista.UserAdmin.Interfaces.Managers
 {
     public interface ISignInManager
     {
-        Task<InvokeResult<UserLoginResponse>> CompleteSignInToAppAsync(AppUser appUser, Stopwatch sw = null, string inviteId = "", string orgId = "");
-
-        Task<InvokeResult<UserLoginResponse>> PasswordSignInAsync(AuthLoginRequest loginRequest);
-
+        Task<InvokeResult<AuthenticationResponse>> CompleteSignInToAppAsync(AppUser appUser, Stopwatch sw = null, string inviteId = "", string orgId = "");
+        Task<InvokeResult<AuthenticationResponse>> PasswordSignInAsync(AuthLoginRequest loginRequest);
         Task SignInAsync(AppUser user, bool isPersistent = false);
-
         Task SignOutAsync();
-
-        /// <summary>
-        /// Method can be called to refresh the user claims
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
         Task RefreshUserLoginAsync(AppUser user);
-    
-       
     }
 }
