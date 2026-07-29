652f5a9c37755a3cea58d346f63d3618056a6dad
Kevin D. Wolf <kevinw@software-logistics.com>
2026-07-29 14:36:38 -0400

    Mark password recovery handler for critical coverage

--- src/LagoVista.UserAdmin/Authentication/Flows/PasswordRecoveryRequestFlowHandler.cs
+++ src/LagoVista.UserAdmin/Authentication/Flows/PasswordRecoveryRequestFlowHandler.cs
@@ -1,9 +1,11 @@
+using LagoVista.Core.Interfaces;
 using LagoVista.UserAdmin.Interfaces.Managers;
 using System;
 using System.Threading.Tasks;
 
 namespace LagoVista.UserAdmin.Authentication.Flows
 {
+    [CriticalCoverage]
     public class PasswordRecoveryRequestFlowHandler : IAuthenticationFlowHandler<PasswordRecoveryRequestFlowRequest>
     {
         public const string TransitionKey = "auth.transition.recovery.request";
