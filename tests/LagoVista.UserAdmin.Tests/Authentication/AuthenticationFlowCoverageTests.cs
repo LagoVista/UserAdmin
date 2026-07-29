78695f26a5b8d6003248ada5a9840de9a9a6b05a
Kevin D. Wolf <kevinw@software-logistics.com>
2026-07-29 14:36:52 -0400

    Add authentication flow coverage discovery stubs

---
+++ tests/LagoVista.UserAdmin.Tests/Authentication/AuthenticationFlowCoverageTests.cs
@@ -0,0 +1,45 @@
+using LagoVista.Core.Interfaces;
+using LagoVista.UserAdmin.Authentication;
+using LagoVista.UserAdmin.Authentication.Flows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace LagoVista.UserAdmin.Tests.Authentication
+{
+    [TestClass]
+    public class AuthenticationFlowCoverageTests
+    {
+        [TestMethod]
+        public void AuthenticationFlowService_Should_Be_Marked_For_Critical_Coverage()
+        {
+            var hasCriticalCoverage = typeof(AuthenticationFlowService)
+                .GetCustomAttributes(typeof(CriticalCoverageAttribute), true)
+                .Any();
+
+            Assert.IsTrue(hasCriticalCoverage);
+        }
+
+        [TestMethod]
+        public void PasswordRecoveryRequestFlowHandler_Should_Be_Marked_For_Critical_Coverage()
+        {
+            var hasCriticalCoverage = typeof(PasswordRecoveryRequestFlowHandler)
+                .GetCustomAttributes(typeof(CriticalCoverageAttribute), true)
+                .Any();
+
+            Assert.IsTrue(hasCriticalCoverage);
+        }
+
+        [TestMethod]
+        public void AuthenticationFlowService_Should_Require_Handler()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new AuthenticationFlowService(null));
+        }
+
+        [TestMethod]
+        public void PasswordRecoveryRequestFlowHandler_Should_Require_Password_Manager()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new PasswordRecoveryRequestFlowHandler(null));
+        }
+    }
+}
