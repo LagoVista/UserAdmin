8cdffceab579957310c68f715470aa3a1635fb47
Kevin D. Wolf <kevinw@software-logistics.com>
2026-07-30 15:54:12 -0400

    Route invitation flow through invariant-safe service

--- src/LagoVista.UserAdmin/Authentication/Flows/InvitationAcceptanceFlowHandler.cs
+++ src/LagoVista.UserAdmin/Authentication/Flows/InvitationAcceptanceFlowHandler.cs
@@ -1,5 +1,4 @@
 using LagoVista.Core.Interfaces;
-using LagoVista.UserAdmin.Interfaces.Managers;
 using LagoVista.UserAdmin.Models.Auth;
 using System;
 using System.Threading.Tasks;
@@ -11,18 +10,18 @@ namespace LagoVista.UserAdmin.Authentication.Flows
     {
         public const string TransitionKey = "auth.transition.invitation.accept";
 
-        private readonly IOrganizationManager _organizationManager;
+        private readonly IInvitationAcceptanceService _invitationAcceptanceService;
 
-        public InvitationAcceptanceFlowHandler(IOrganizationManager organizationManager)
+        public InvitationAcceptanceFlowHandler(IInvitationAcceptanceService invitationAcceptanceService)
         {
-            _organizationManager = organizationManager ?? throw new ArgumentNullException(nameof(organizationManager));
+            _invitationAcceptanceService = invitationAcceptanceService ?? throw new ArgumentNullException(nameof(invitationAcceptanceService));
         }
 
         public async Task<AuthenticationFlowResult<AcceptInviteResponse>> HandleAsync(InvitationAcceptanceFlowRequest request)
         {
             if (request == null) throw new ArgumentNullException(nameof(request));
 
-            var result = await _organizationManager.AcceptInvitationAsync(request.InviteId, request.UserId);
+            var result = await _invitationAcceptanceService.AcceptInvitationAsync(request.InviteId, request.UserId);
             return new AuthenticationFlowResult<AcceptInviteResponse>(TransitionKey, result);
         }
     }
