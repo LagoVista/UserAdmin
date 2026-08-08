fbfd28cb1f624e7eb59e0088c9791077e7f7ae24
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-08 12:42:03 -0400

    seed authentication behavior universe

--- src/LagoVista.UserAdmin.Repos/Startup.cs
+++ src/LagoVista.UserAdmin.Repos/Startup.cs
@@ -91,11 +91,6 @@ namespace LagoVista.UserAdmin.Repos
             services.AddScoped<IAppUserPasskeyCredentialRepo, PasskeyCredentialRepo>();
             services.AddScoped<IPasskeyCredentialIndexRepo, PasskeyCredentialIndexRepo>();
             services.AddScoped<IProvisionalEnvironmentEntityRepo, ProvisionalEnvironmentEntityRepo>();
-            services.AddScoped<IProvisionalEnvironmentRecoveryIndexRepo, ProvisionalEnvironmentRecoveryIndexRepo>();
-            services.AddScoped<IProvisionalEnvironmentInstallationIndexRepo, ProvisionalEnvironmentInstallationIndexRepo>();
-            services.AddScoped<IProvisionalEnvironmentCreationIndexRepo, ProvisionalEnvironmentCreationIndexRepo>();
-            services.AddScoped<IProvisionalEnvironmentStateIndexRepo, ProvisionalEnvironmentStateIndexRepo>();
-            services.AddScoped<IProvisionalEnvironmentRepo, ProvisionalEnvironmentRepo>();
             services.AddTransient<ITestArtifactStorage, TestArtifactStorage>();
             services.AddTransient<IPasskeyChallengeStore, RedisPasskeyChallengeStore>();
             services.AddTransient<IMagicLinkAttemptStore, MagicLinkAttemptCacheStore>();
