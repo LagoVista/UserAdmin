8f5776864707005609428c9736481c172969c313
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-17 15:33:39 -0400

    Cover provisional organization first-user bootstrap

---
+++ tests/LagoVista.UserAdmin.Auth.Tests/ProvisionalOrganizationBootstrapTests.cs
@@ -0,0 +1,100 @@
+using LagoVista.Core.Interfaces;
+using LagoVista.Core.PlatformSupport;
+using LagoVista.UserAdmin.Interfaces;
+using LagoVista.UserAdmin.Interfaces.Managers;
+using LagoVista.UserAdmin.Interfaces.Repos.Orgs;
+using LagoVista.UserAdmin.Interfaces.Repos.Security;
+using LagoVista.UserAdmin.Interfaces.Repos.Users;
+using LagoVista.UserAdmin.Managers;
+using LagoVista.UserAdmin.Models.Orgs;
+using LagoVista.UserAdmin.Models.Users;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LagoVista.UserAdmin.Auth.Tests
+{
+    [TestFixture]
+    public class ProvisionalOrganizationBootstrapTests
+    {
+        [Test]
+        public async Task CreateProvisionalOrganizationAsync_Should_Bootstrap_First_User_Without_Rereading_Organization()
+        {
+            const string appUserId = "provisional-user-id";
+            const string organizationId = "provisional-organization-id";
+
+            var organizationRepo = new Mock<IOrganizationRepo>();
+            var orgUserRepo = new Mock<IOrgUserRepo>();
+            var appUserRepo = new Mock<IAppUserRepo>();
+            var userRoleManager = new Mock<IUserRoleManager>();
+            var coreAppServices = new Mock<ICoreAppServices>();
+
+            organizationRepo
+                .Setup(repo => repo.QueryOrganizationExistAsync(organizationId))
+                .ReturnsAsync(false);
+            organizationRepo
+                .Setup(repo => repo.AddOrganizationAsync(It.IsAny<Organization>()))
+                .Returns(Task.CompletedTask);
+            orgUserRepo
+                .Setup(repo => repo.QueryOrgHasUserAsync(organizationId, appUserId))
+                .ReturnsAsync(false);
+            orgUserRepo
+                .Setup(repo => repo.AddOrgUserAsync(It.IsAny<OrgUser>()))
+                .Returns(Task.CompletedTask);
+            appUserRepo
+                .Setup(repo => repo.UpdateAsync(It.IsAny<AppUser>()))
+                .Returns(Task.CompletedTask);
+            userRoleManager
+                .Setup(manager => manager.UserHasRoleAsync(It.IsAny<string>(), appUserId, organizationId))
+                .ReturnsAsync(true);
+            coreAppServices
+                .SetupGet(services => services.Logger)
+                .Returns(Mock.Of<ILogger>());
+
+            var manager = new OrgManager(
+                organizationRepo.Object,
+                Mock.Of<IOrgLocationRepo>(),
+                orgUserRepo.Object,
+                appUserRepo.Object,
+                Mock.Of<IInviteUserRepo>(),
+                Mock.Of<ILocationUserRepo>(),
+                Mock.Of<ILocationRoleRepo>(),
+                Mock.Of<IEmailSender>(),
+                Mock.Of<IOrgInitializer>(),
+                new DefaultRoleList(),
+                Mock.Of<IOwnedObjectRepo>(),
+                Mock.Of<IUserRoleRepo>(),
+                userRoleManager.Object,
+                Mock.Of<IAuthenticationLogManager>(),
+                Mock.Of<ISubscriptionManager>(),
+                Mock.Of<ILocationDiagramRepo>(),
+                Mock.Of<IRoleRepo>(),
+                Mock.Of<ISecureStorage>(),
+                coreAppServices.Object,
+                Mock.Of<IOrgInformationSource>());
+
+            var appUser = new AppUser(null, "provisional-user", "Provisional Environment")
+            {
+                Id = appUserId,
+                IsAnonymous = true,
+                Organizations = new List<LagoVista.Core.Models.EntityHeader>()
+            };
+
+            var result = await manager.CreateProvisionalOrganizationAsync(appUser, organizationId);
+
+            Assert.That(result.Successful, Is.True);
+            Assert.That(result.Result.Id, Is.EqualTo(organizationId));
+            Assert.That(appUser.CurrentOrganization.Id, Is.EqualTo(organizationId));
+            Assert.That(appUser.Organizations, Has.One.Matches<LagoVista.Core.Models.EntityHeader>(org => org.Id == organizationId));
+
+            organizationRepo.Verify(repo => repo.AddOrganizationAsync(It.Is<Organization>(org => org.Id == organizationId)), Times.Once);
+            organizationRepo.Verify(repo => repo.GetOrganizationAsync(It.IsAny<string>()), Times.Never);
+            orgUserRepo.Verify(repo => repo.AddOrgUserAsync(It.Is<OrgUser>(orgUser =>
+                orgUser.OrganizationId == organizationId &&
+                orgUser.UserId == appUserId &&
+                orgUser.IsOrgAdmin &&
+                orgUser.IsAppBuilder)), Times.Once);
+        }
+    }
+}
