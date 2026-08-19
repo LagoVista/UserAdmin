bc71298bb959de4337ffbc7ca952355567336b9a
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-19 17:19:28 -0400

    Register canonical sign-out handler

--- src/LagoVista.UserAdmin/Startup.cs
+++ src/LagoVista.UserAdmin/Startup.cs
@@ -57,6 +57,7 @@ namespace LagoVista.UserAdmin
             services.AddScoped<IFunctionMapManager, FunctionMapManager>();
             services.AddScoped<IPasswordLoginFlowHandler, PasswordLoginFlowHandler>();
             services.AddScoped<ITotpAuthenticationFlowHandler, TotpAuthenticationFlowHandler>();
+            services.AddScoped<ISignOutFlowHandler, SignOutFlowHandler>();
             services.AddScoped<IAuthenticationFlowHandler<PasswordChangeFlowRequest>, PasswordChangeFlowHandler>();
             services.AddScoped<IAuthenticationFlowHandler<PasswordRecoveryRequestFlowRequest>, PasswordRecoveryRequestFlowHandler>();
             services.AddScoped<IAuthenticationFlowHandler<PasswordRecoveryVerificationFlowRequest, string>, PasswordRecoveryVerificationFlowHandler>();
