665e84259045e3eaa24c4dfc62481cef8c9b188e
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-19 16:09:53 -0400

    Test request-scoped provisional bootstrap marker

--- tests/LagoVista.UserAdmin.Auth.Tests/ProvisionalOrganizationBootstrapContextTests.cs
+++ tests/LagoVista.UserAdmin.Auth.Tests/ProvisionalOrganizationBootstrapContextTests.cs
@@ -1,5 +1,6 @@
 using LagoVista.UserAdmin.Interfaces;
 using NUnit.Framework;
+using System.Diagnostics;
 
 namespace LagoVista.UserAdmin.Auth.Tests
 {
@@ -12,15 +13,36 @@ namespace LagoVista.UserAdmin.Auth.Tests
             const string organizationId = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
             const string userId = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
 
-            ProvisionalOrganizationBootstrapContext.MarkFresh(organizationId, userId);
+            using (var activity = new Activity("provisional-bootstrap-test").Start())
+            {
+                ProvisionalOrganizationBootstrapContext.MarkFresh(organizationId, userId);
 
-            Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeRoleProbe("OTHER", userId), Is.False);
-            Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeRoleProbe(organizationId, userId), Is.True);
-            Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeRoleProbe(organizationId, userId), Is.False);
+                Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeRoleProbe("OTHER", userId), Is.False);
+                Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeRoleProbe(organizationId, userId), Is.True);
+                Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeRoleProbe(organizationId, userId), Is.False);
 
-            Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeMembershipProbe(organizationId, "OTHER"), Is.False);
-            Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeMembershipProbe(organizationId, userId), Is.True);
-            Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeMembershipProbe(organizationId, userId), Is.False);
+                Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeMembershipProbe(organizationId, "OTHER"), Is.False);
+                Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeMembershipProbe(organizationId, userId), Is.True);
+                Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeMembershipProbe(organizationId, userId), Is.False);
+            }
+        }
+
+        [Test]
+        public void FreshBootstrapProbes_Should_Not_Cross_Request_Trace()
+        {
+            const string organizationId = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC";
+            const string userId = "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD";
+
+            using (var firstActivity = new Activity("provisional-bootstrap-first").Start())
+            {
+                ProvisionalOrganizationBootstrapContext.MarkFresh(organizationId, userId);
+            }
+
+            using (var secondActivity = new Activity("provisional-bootstrap-second").Start())
+            {
+                Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeRoleProbe(organizationId, userId), Is.False);
+                Assert.That(ProvisionalOrganizationBootstrapContext.TryConsumeMembershipProbe(organizationId, userId), Is.False);
+            }
         }
     }
 }
