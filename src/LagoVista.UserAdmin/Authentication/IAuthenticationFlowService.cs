ba31bd506741819db0e6553c44267cfeb0c6dd7a
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-19 15:19:38 -0400

    Expose TOTP enrollment through authentication flow service

--- src/LagoVista.UserAdmin/Authentication/IAuthenticationFlowService.cs
+++ src/LagoVista.UserAdmin/Authentication/IAuthenticationFlowService.cs
@@ -2,6 +2,7 @@ using LagoVista.Core.Models;
 using LagoVista.Core.Validation;
 using LagoVista.UserAdmin.Models.Auth;
 using LagoVista.UserAdmin.Models.DTOs;
+using LagoVista.UserAdmin.Models.Users;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -17,6 +18,8 @@ namespace LagoVista.UserAdmin.Authentication
         Task<InvokeResult<AcceptInviteResponse>> AcceptInvitationAsync(string inviteId, string userId);
         Task<InvokeResult<EmailVerificationSendResult>> SendEmailVerificationCodeAsync(EntityHeader user);
         Task<InvokeResult> VerifyEmailAsync(ConfirmEmail request, EntityHeader user);
+        Task<InvokeResult<AppUserTotpEnrollmentInfo>> BeginTotpEnrollmentAsync(string userId, EntityHeader organization, EntityHeader user);
+        Task<InvokeResult<List<string>>> ConfirmTotpEnrollmentAsync(string userId, string totp, EntityHeader organization, EntityHeader user);
         Task<InvokeResult> TurnOffTotpAsync(string userId, EntityHeader organization, EntityHeader user);
         Task<InvokeResult<List<string>>> RotateTotpRecoveryCodesAsync(string userId, EntityHeader organization, EntityHeader user);
     }
