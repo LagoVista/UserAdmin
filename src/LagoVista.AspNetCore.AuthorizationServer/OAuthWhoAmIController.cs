737cefb431efbfba252ff34700e3ec1f676a0312
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-19 12:29:32 -0400

    Fix OAuth whoami resource reporting

--- src/LagoVista.AspNetCore.AuthorizationServer/OAuthWhoAmIController.cs
+++ src/LagoVista.AspNetCore.AuthorizationServer/OAuthWhoAmIController.cs
@@ -13,13 +13,22 @@ namespace LagoVista.AspNetCore.AuthorizationServer
         [HttpGet("/api/oauth/whoami")]
         public IActionResult GetWhoAmI()
         {
+            var audiences = User.GetAudiences().ToArray();
+
             return Ok(new
             {
                 Subject = User.GetClaim(Claims.Subject),
                 Name = User.GetClaim(Claims.Name),
                 ClientId = User.GetClaim(Claims.ClientId),
                 Scopes = User.GetScopes().ToArray(),
-                Resources = User.GetResources().ToArray(),
+
+                // OpenIddict maps the resources selected for an authorization principal
+                // to the access token audience (aud) claim. Once the access token has
+                // been validated, GetAudiences() is therefore the authoritative view of
+                // the resource servers this token was issued for.
+                Resources = audiences,
+                Audiences = audiences,
+
                 Claims = User.Claims.Select(claim => new
                 {
                     claim.Type,
