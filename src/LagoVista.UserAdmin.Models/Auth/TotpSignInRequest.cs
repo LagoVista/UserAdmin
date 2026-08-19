98a5b3a2479372d3a41a240e5e38c0a3623e5b30
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-19 15:41:03 -0400

    Add TOTP sign-in request contract

---
+++ src/LagoVista.UserAdmin.Models/Auth/TotpSignInRequest.cs
@@ -0,0 +1,9 @@
+namespace LagoVista.UserAdmin.Models.Auth
+{
+    public class TotpSignInRequest
+    {
+        public string Email { get; set; }
+        public string Totp { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
