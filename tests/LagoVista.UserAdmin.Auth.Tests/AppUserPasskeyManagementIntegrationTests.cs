f5ccdcf274ac6318fedaa09c0611c2ba5d80d8f7
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-20 15:26:25 -0400

    Fix passkey management test imports

--- tests/LagoVista.UserAdmin.Auth.Tests/AppUserPasskeyManagementIntegrationTests.cs
+++ tests/LagoVista.UserAdmin.Auth.Tests/AppUserPasskeyManagementIntegrationTests.cs
@@ -15,6 +15,7 @@ using Moq;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
