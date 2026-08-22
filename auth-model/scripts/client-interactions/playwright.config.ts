5703ba455035e7e18202c94a218e35fd267f89d0
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-22 16:56:06 -0400

    Slow headed Client Interaction Playwright runs

--- auth-model/scripts/client-interactions/playwright.config.ts
+++ auth-model/scripts/client-interactions/playwright.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from '@playwright/test';
 
-export default defineConfig({ testDir: './specs', use: { baseURL: 'http://localhost:4200', headless: true }, reporter: [['./reporter.ts']], timeout: 15000 });
+export default defineConfig({ testDir: './specs', use: { baseURL: 'http://localhost:4200', headless: false, launchOptions: { slowMo: 350 } }, reporter: [['./reporter.ts']], timeout: 15000 });
