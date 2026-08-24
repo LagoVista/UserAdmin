e3697b4aa59fd2dbb646136fefd530e359bdb657
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-24 10:40:10 -0400

    Make authentication log queries org scoped

--- src/LagoVista.UserAdmin/Interfaces/Repos/Security/IAuthenticationLogRepo.cs
+++ src/LagoVista.UserAdmin/Interfaces/Repos/Security/IAuthenticationLogRepo.cs
@@ -8,15 +8,12 @@ using System.Threading.Tasks;
 
 namespace LagoVista.UserAdmin.Interfaces.Repos.Security
 {
-
     public interface IAuthenticationLogRepo
     {
         Task AddAsync(AuthenticationLog authLog);
-        Task<ListResponse<AuthenticationLog>> GetAllAsync(ListRequest listRequest);
-        Task<ListResponse<AuthenticationLog>> GetForUserNameAsync(string userName, ListRequest listRequest);
-        Task<ListResponse<AuthenticationLog>> GetForUserIdAsync(string userId, ListRequest listRequest);
-        Task<ListResponse<AuthenticationLog>> GetAllAsync(string orgId, ListRequest listRequest);
-        Task<ListResponse<AuthenticationLog>> GetAsync(AuthLogTypes type, ListRequest listRequest);
-        Task<ListResponse<AuthenticationLog>> GetAsync(string orgId, AuthLogTypes type, ListRequest listRequest);
+        Task<ListResponse<AuthenticationLog>> GetAllAsync(string organizationId, ListRequest listRequest);
+        Task<ListResponse<AuthenticationLog>> GetForUserNameAsync(string organizationId, string userName, ListRequest listRequest);
+        Task<ListResponse<AuthenticationLog>> GetForUserIdAsync(string organizationId, string userId, ListRequest listRequest);
+        Task<ListResponse<AuthenticationLog>> GetAsync(string organizationId, AuthLogTypes type, ListRequest listRequest);
     }
 }
